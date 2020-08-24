fe016da4db9c2358bcda07f6ce00cf7e427d3575
Harrison Hascoe <hascoeharrison@gmail.com>
2020-08-24 18:49:19 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,13 +1,29 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
-  board= " |    |"
+  puts " |    |"
+  puts "--------"
+  puts " |    |"
+  puts "--------"
+  puts " |    |"
   puts "Welcome to Tik Tac Toe"
-  puts board
-  puts "What move would x liek to make (1-9)?"
+  puts " |    |"
+  puts "--------"
+  puts " |    |"
+  puts "--------"
+  puts " |    |"
+  puts "What move would x like to make (1-9)?"
   puts ">5"
-  puts [" "," "," "," ","X"," "," "," "," "]
+  puts " |    |"
+  puts "--------"
+  puts " |  X |"
+  puts "--------"
+  puts " |    |"
   puts "What move would O like to make (1-9)?"
   puts ">1"
-  puts ["O"," "," "," ","X"," "," "," "," "]
+  puts "O|    |"
+  puts "--------"
+  puts " |  X |"
+  puts "--------"
+  puts " |    |"
   puts "etc..."
 end
